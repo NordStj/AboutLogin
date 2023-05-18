ba48e50c952d87989b05ff85fe9bb7597eeace36
NordStj <Neoskyping@gmail.com>
2023-05-18 11:27:56 +0300

    final result

--- LoginScreen/ViewControllers/AboutViewController.swift
+++ LoginScreen/ViewControllers/AboutViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
-    @IBOutlet var label: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var ageLabel: UILabel!
+    @IBOutlet var favoriteLabel: UILabel!
+    @IBOutlet var eatLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameLabel.text = "My name is \(name)"
+        ageLabel.text = "I'm \(age) years old."
+        favoriteLabel.text = "My favorite coffee is \(favorit)"
+        eatLabel.text = "I love \(eat)"
 
     }
 
